15a5985dc911094f3e573fda998c7899dd324921
jeevan johnson <jeevanjohnson709@gmail.com>
2024-07-01 21:02:33 -0700

    no more fetch & auto launch option

--- static/onboarding.js
+++ static/onboarding.js
@@ -1,6 +1,12 @@
-const OSU_PATH_URL_LINK = "http://localhost:5001/osu_path/";
-const API_SERVER_CONFIGURATION_UPDATE_URL = "http://localhost:5000/api/v1/config/update";
-const KILL_CLIENT_URL = "http://localhost:5001/application/kill";
+//const axios = require('axios');
+
+const OSU_CLIENT_SERVICE_ROOT_URL = "http://localhost:5001";
+const API_SERVER_SERVICE_ROOT_URL = "http://localhost:5000";
+
+
+const OSU_PATH_URL_LINK = `${OSU_CLIENT_SERVICE_ROOT_URL}/osu_path`;
+const API_SERVER_CONFIGURATION_UPDATE_URL = `${API_SERVER_SERVICE_ROOT_URL}/api/v1/config/update`;
+const KILL_CLIENT_URL = `${OSU_CLIENT_SERVICE_ROOT_URL}/application/kill`;
 
 const sendData = async () => {
     const form = document.querySelector('form');
@@ -12,6 +18,7 @@ const sendData = async () => {
         jsonObject[key] = value;
     }
 
+    /* TODO: Implement this on the other service
     const osu_path_response = await fetch(OSU_PATH_URL_LINK);
 
     if (osu_path_response.status >= 400) {
@@ -20,16 +27,19 @@ const sendData = async () => {
     }
 
     const osu_path_data = await osu_path_response.json();
-    jsonObject['osu_path'] = osu_path_data.osu_path;
+    jsonObject['osu_path'] = osu_path_data.osu_path;*/
 
     // update api server configuration
-    const api_server_update_configuration_response = await fetch(API_SERVER_CONFIGURATION_UPDATE_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonObject)
-    });
+
+    let api_server_update_configuration_response;
+    try {
+        api_server_update_configuration_response = await axios.post(API_SERVER_CONFIGURATION_UPDATE_URL, jsonObject,
+            { headers: { 'Content-Type': 'application/json' } }
+        );
+    } catch (error) {
+        alert(error.response);
+        return
+    }
 
     const api_server_update_configuration_response_data = await api_server_update_configuration_response.json();
 
@@ -40,6 +50,7 @@ const sendData = async () => {
     }
 
     // now kill the client
+    /* TODO: Implement this on the other service
     const kill_response = await fetch(KILL_CLIENT_URL, {
         method: 'POST'
     });
@@ -49,7 +60,7 @@ const sendData = async () => {
     if (kill_response.status >= 400) {
         alert(kill_response_data.error_message);
         return;
-    }
+    }*/
 
     // redirect to the dashboard
     window.location.href = "/";

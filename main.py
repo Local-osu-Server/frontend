db25804100e392e1c54f77bdcd9858a28c879835
jeevan johnson <jeevanjohnson709@gmail.com>
2024-07-02 22:37:33 -0700

    properly parsed the form data to a json object and ensured default values were set

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi.staticfiles import StaticFiles
 
 import config
 from routers.frontend import frontend_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -45,6 +46,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, debug=True)
+
+# TODO: understand this solution, https://github.com/tiangolo/fastapi/issues/1663
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(
--- static/onboarding.js
+++ static/onboarding.js
@@ -4,7 +4,7 @@ const OSU_CLIENT_SERVICE_ROOT_URL = "http://localhost:5001";
 const API_SERVER_SERVICE_ROOT_URL = "http://localhost:5000";
 
 
-const OSU_PATH_URL_LINK = `${OSU_CLIENT_SERVICE_ROOT_URL}/osu_path`;
+const OSU_FOLDER_PATH_URL_LINK = `${OSU_CLIENT_SERVICE_ROOT_URL}/application/path`;
 const API_SERVER_CONFIGURATION_UPDATE_URL = `${API_SERVER_SERVICE_ROOT_URL}/api/v1/config/update`;
 const KILL_CLIENT_URL = `${OSU_CLIENT_SERVICE_ROOT_URL}/application/kill`;
 
@@ -12,28 +12,53 @@ const sendData = async () => {
     const form = document.querySelector('form');
     const formData = new FormData(form);
 
-    const jsonObject = {};
+    const configBody = {};
 
     for (const [key, value] of formData.entries()) {
-        jsonObject[key] = value;
+        if (
+            key === 'display_pp_on_leaderboard' ||
+            key === 'rank_scores_by_pp_or_score' ||
+            key === 'allow_pp_from_modified_maps'
+        ) {
+            configBody[key] = value === 'on';
+            continue;
+        }
+
+        // convert value to an int
+        if (key === 'num_scores_seen_on_leaderboards' || key === "osu_api_v2_client_id") {
+            configBody[key] = parseInt(value);
+            continue;
+        }
+
+        configBody[key] = value;
     }
 
-    /* TODO: Implement this on the other service
-    const osu_path_response = await fetch(OSU_PATH_URL_LINK);
+    // if display_pp_on_leaderboard or rank_scores_by_pp_or_score isn't set, set it to false
+    if (!configBody.hasOwnProperty('display_pp_on_leaderboard')) {
+        configBody['display_pp_on_leaderboard'] = false;
+    }
+    if (!configBody.hasOwnProperty('rank_scores_by_pp_or_score')) {
+        configBody['rank_scores_by_pp_or_score'] = false;
+    }
+    if (!configBody.hasOwnProperty('allow_pp_from_modified_maps')) {
+        configBody['allow_pp_from_modified_maps'] = false;
+    }
 
-    if (osu_path_response.status >= 400) {
+    // TODO: Implement this on the other service
+    const osu_folder_path_response = await axios.get(OSU_FOLDER_PATH_URL_LINK);
+
+    if (osu_folder_path_response.status >= 400) {
         alert("Could not get osu! path because osu! isn't opened.");
         return;
     }
 
-    const osu_path_data = await osu_path_response.json();
-    jsonObject['osu_path'] = osu_path_data.osu_path;*/
+    configBody['osu_folder_path'] = osu_folder_path_response.data.path;
 
     // update api server configuration
 
     let api_server_update_configuration_response;
     try {
-        api_server_update_configuration_response = await axios.post(API_SERVER_CONFIGURATION_UPDATE_URL, jsonObject,
+        api_server_update_configuration_response = await axios.post(API_SERVER_CONFIGURATION_UPDATE_URL, configBody,
             { headers: { 'Content-Type': 'application/json' } }
         );
     } catch (error) {
@@ -41,7 +66,7 @@ const sendData = async () => {
         return
     }
 
-    const api_server_update_configuration_response_data = await api_server_update_configuration_response.json();
+    const api_server_update_configuration_response_data = api_server_update_configuration_response.data;
 
     // Send the specific error message that the server sends
     if (api_server_update_configuration_response.status >= 400) {
